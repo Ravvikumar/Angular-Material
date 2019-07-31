b38c0bade514028ced37145ba9dae80f9625bd91
Ravi Kumar <ce988@ce-988.cssad.compassitesinc.com>
2019-07-31 08:20:24 +0530

    Angular-Material Practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Angular-Material';
+  Notifications = 0;
+  showSpinner = false;
+  opened = false;
+  selectedValue: string;
+
+  loadData() {
+    this.showSpinner = true;
+    setTimeout(() =>{
+      this.showSpinner = false;
+    },5000)
+  }
+
+  logChange(index) {
+    console.log(index);
+  }
+
+  options: string[] = ['Angular', 'Recat', 'Vew'];
+  objectOptions = [
+    { name: 'Angular' },
+    { name: 'Angular Material' },
+    { name: 'React' },
+    { name: 'Vew' }
+  ];
+
+  displayFunc(subject) {
+    return subject ? subject.name : undefined; 
+  }
+
+  myControl = new FormControl();
+  filteredOptions: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(""),
+      map( value => this._filter(value) )
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase()
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, 
+         MatButtonToggleModule, 
+         MatIconModule, 
+         MatProgressSpinnerModule, 
+         MatToolbarModule, 
+         MatSidenavModule, 
+         MatMenuModule,
+         MatListModule,
+         MatDividerModule,
+         MatGridListModule,
+         MatExpansionModule,
+         MatCardModule,
+         MatTabsModule,
+         MatStepperModule,
+         MatFormFieldModule,
+         MatInputModule,
+         MatSelectModule,
+         MatAutocompleteModule
+        } from '@angular/material';
+import { MatBadgeModule } from '@angular/material/badge'
+
+const Material = [
+  MatButtonModule, 
+  MatButtonToggleModule, 
+  MatIconModule, 
+  MatBadgeModule, 
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatMenuModule,
+  MatListModule,
+  MatDividerModule,
+  MatGridListModule,
+  MatExpansionModule,
+  MatCardModule,
+  MatTabsModule,
+  MatStepperModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatAutocompleteModule
+]
+@NgModule({
+  imports: [Material],
+  exports: [Material]
+})
+export class MaterialModule { }
